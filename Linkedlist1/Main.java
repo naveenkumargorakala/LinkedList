5cf9a199db1fc5423d716e7cd5b36d2be0214661
naveenkumargorakala <nicenaveengorakala@gmail.com>
2023-03-29 09:22:05 +0530

    Added: Adding elements to LinkedList

---
+++ Linkedlist1/Main.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.DataStructures.Linkedlist;
+
+public class Main<T> {
+    public static void main(String[] args) {
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        System.out.println("pushing of elements: ");
+        linkedList.push(70);
+        linkedList.push(30);
+        linkedList.push(56);
+        linkedList.show();
+    }
+}
